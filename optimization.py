7aefe71815adb8518751ea1632ed9d632f9bbc83
Isaac Kim <ikim@Isaacs-MacBook-Pro-3.local>
2018-11-05 13:42:25 -0800

    Added SPSA optimization

--- optimization.py
+++ optimization.py
@@ -59,7 +59,37 @@ def qaoa_maxcut_opt(edges, angles, sample_size,
                 shots=shots)
             current_value = qaoa_maxcut(edges, angles, 1000)
             print(
-                "{}th sweep, {}th angle update: {}".format(
-                    i, j, current_value))
+                "{}th sweep, {}th angle update, sample size={}: {}".format(
+                    i, j, shots * sample_size_reduced * (i * len(angles) + j),current_value))
 
+    print("Total number of samples: {}".format(shots * sample_size_reduced * sweeps * len(angles)))
     return angles
+
+def spsa_maxcut_opt(edges, angles, shots, alpha = 0.602, gamma=0.101, itt=1000):
+    """
+    Optimizing a MAXCUT problem with SPSA.
+
+    Args:
+        edges (list): Edges that define the graph of the MAXCUT problem.
+        angles (list): QAOA angles.
+        shots (int): Total number of single-shot measurements.
+        alpha (float): Parameter for SPSA
+        gamma (float): Parameter for SPSA
+        itt (int): Total number of iterations
+    """
+
+    for k in range(itt):
+        c_k = 0.01 / pow((1+k), gamma)
+        a_k = 0.01 / pow((1+k), alpha)
+        randombit = np.random.randint(2, size=len(angles))
+        randpm = 2* randombit - 1
+        angles_p = angles + c_k * randpm
+        angles_m = angles - c_k * randpm
+        g_k = (qaoa_maxcut(edges, angles_p, shots=shots) - qaoa_maxcut(edges, angles_m)) / (2*c_k)
+
+        angles = [angle + g_k * mybit for (angle, mybit) in zip(angles,randpm)]
+        total_shots = k * shots * 2
+        print("Total shots = {}: {}".format(total_shots, qaoa_maxcut(edges, angles)))
+
+#def powell_maxcut_opt(edges, angles, shots):
+    
